b0ce1caea30a5a70a9aafeb7584e70acb9e67522
Caleb Ebers <calebebers@gmail.com>
2025-03-07 18:40:13 -0600

    enable terser-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
-
-
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+    
     return {
         entry: './src/index.js',
         output: {
             filename: 'bundle.js',
             path: path.resolve(__dirname, 'dist'),
-            library: 'Ahu3D',  // Explicitly name the library
+            library: 'Ahu3D',
             libraryTarget: 'umd',
-            globalObject: 'this',  // Ensure compatibility in both browser and Node.js environments
+            globalObject: 'this',
             publicPath: 'auto',
         },
         module: {
             rules: [
                 {
-                    test: /\.js$/, // Apply Babel to JavaScript files
+                    test: /\.js$/,
                     exclude: /node_modules/,
                     use: {
                         loader: 'babel-loader',
@@ -26,7 +27,7 @@ module.exports = (env, argv) => {
                     }
                 },
                 {
-                    test: /\.json$/, // Allow importing JSON files
+                    test: /\.json$/,
                     type: 'javascript/auto',
                     loader: 'json-loader'
                 },
@@ -43,7 +44,7 @@ module.exports = (env, argv) => {
                     test: /\.svg$/,
                     use: [
                         {
-                            loader: 'raw-loader', // Loads SVG as raw HTML string
+                            loader: 'raw-loader',
                         },
                     ],
                 }
@@ -54,6 +55,37 @@ module.exports = (env, argv) => {
             fallback: {
                 path: false,
             },
+        },
+        optimization: {
+            minimize: isProduction,
+            minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        parse: {
+                            ecma: 8,
+                        },
+                        compress: {
+                            ecma: 5,
+                            warnings: false,
+                            comparisons: false,
+                            inline: 2,
+                            drop_console: isProduction,
+                            drop_debugger: isProduction,
+                            pure_funcs: isProduction ? ['console.info', 'console.debug', 'console.log'] : [],
+                        },
+                        mangle: {
+                            safari10: true,
+                        },
+                        output: {
+                            ecma: 5,
+                            comments: false,
+                            ascii_only: true,
+                        },
+                    },
+                    extractComments: false,
+                    parallel: true,
+                }),
+            ],
         },
     };
 };
